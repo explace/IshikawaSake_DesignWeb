235d6f8bbd559fb45082fd534f866eff4ed00238
jahid28 <jahidrhps123@gmail.com>
2024-09-10 22:06:16 +0530

    news animation

--- app/News/page.js
+++ app/News/page.js
@@ -1,13 +1,62 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 const News = () => {
+
+  const fadeIn = useRef(null);
+
+  useEffect(() => {
+    const target = fadeIn.current;
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          // target.classList.add("fadeIn");
+          // target.classList.style.animation="fadeInNewsAni 3s forwards ease-out"
+          document.querySelectorAll(".fadeInNews").forEach((item, index) => {
+            item.style.animation = `fadeInNewsAni .6s forwards ease-out ${index/10}s`;
+          });
+          document.querySelector(".fadeInNewsLine").style.animation = `fadeInNewsLineAni .6s forwards ease-out 1.5s`
+        }
+        else {
+          // Optionally, remove the class when it leaves the viewport
+          // target.classList.remove("fadeIn");
+          document.querySelectorAll(".fadeInNews").forEach((item, index) => {
+            item.style.animation = "none"
+          });
+          document.querySelector(".fadeInNewsLine").style.animation = "none";
+        
+        }
+      });
+    });
+
+    if (target) {
+      observer.observe(target);
+    }
+    // Cleanup observer when the component unmounts
+    return () => {
+      if (target) {
+        observer.unobserve(target);
+      }
+    };
+  }, []);
+
   return (
     <div className="mt-10 place-items-center border-0 border-red-500">
-      <div className="flex items-center justify-center mb-6">
-        <p className="text-4xl mr-3">NEWS</p>
-        <p className="text-lg mr-3">Update</p>
-        <p className="w-[65%] h-[2px] bg-black"></p>
-      </div>
+     <div ref={fadeIn} className="flex items-center justify-center mb-6">
+        <div className="text-4xl mr-3 flex items-center">
+          <p className="fadeInNews">N</p>
+          <p className="fadeInNews">E</p>
+          <p className="fadeInNews">W</p>
+          <p className="fadeInNews">S</p>
+        </div>
+        <div className="text-lg mr-3 flex items-center">
+          <p className="fadeInNews">U</p>
+          <p className="fadeInNews">p</p>
+          <p className="fadeInNews">d</p>
+          <p className="fadeInNews">a</p>
+          <p className="fadeInNews">t</p>
+          <p className="fadeInNews">e</p>
+        </div>
+        <p className="w-[65%] h-[2px] bg-black fadeInNewsLine"></p></div>
 
       <div className="w-[100%] grid place-items-center">
       <div className="w-[77%]">
